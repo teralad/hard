9d415d4a1dbd9630e1532f357c834b1a6bd62398
Varun Kumaar <teralad@gmail.com>
2018-08-20 19:50:05 +0530

    route to login if user is not signed in

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,11 @@
 class VisitorsController < ApplicationController
+
+    def index
+        @user = current_user
+        if !user_signed_in?
+            redirect_to new_user_session_path
+        else
+            redirect_to user_summary_path
+        end
+    end
 end
