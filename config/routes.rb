76f3e8b4095e49ebe932b1959f63d2b51549e5d5
Varun Kumaar <teralad@gmail.com>
2018-08-25 19:10:20 +0530

    removing graphiql endpoint as its not suported in prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-
   post "/graphql", to: "graphql#execute"
   root to: 'visitors#index'
   devise_for :users
   resources :users
-
   resources :answers
   resources :questions
   resources :exams do
